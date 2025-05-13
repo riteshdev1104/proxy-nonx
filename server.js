e5ab3a386e0f14a76ed98c7421c1050aeffffa6d
riteshdev1104 <riteshyt92173@gmail.com>
2025-05-13 12:16:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
-const express = require("express");
-const Unblocker = require("unblocker");
-const path = require("path");
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const path = require('path');
 
 const app = express();
-const unblocker = Unblocker({
-  prefix: "/proxy/",
-  responseMiddleware: [
-    // optional: remove CSP to allow iframe, JS, etc.
-    (data, req, res, next) => {
-      delete res.headers["content-security-policy"];
-      next();
-    },
-  ]
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.use(express.static("public"));
-app.use(unblocker);
+app.use('/proxy', createProxyMiddleware({
+  target: 'https://www.google.com',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/proxy': '',
+  },
+}));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log("Proxy running at port " + port);
+  console.log(`Proxy server running on port ${port}`);
 });
